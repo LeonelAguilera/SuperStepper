1938512777606e9e6dec5a385e2835bdc47fe3d5
LeonelAguilera <64066370+LeonelAguilera@users.noreply.github.com>
2022-10-28 13:52:57 +0200

    Fixed variable names

--- BaseStepper/BaseStepper.ino
+++ BaseStepper/BaseStepper.ino
@@ -43,17 +43,17 @@ bool micropasos[8][4] = {
 int T = 500;
 
 void setup() {
-  pinMode(A1, OUTPUT);
-  pinMode(A2, OUTPUT);
-  pinMode(B1, OUTPUT);
-  pinMode(B2, OUTPUT);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(A1, micropasos[i%8][0]);
-  digitalWrite(A2, micropasos[i%8][1]);
-  digitalWrite(B1, micropasos[i%8][2]);
-  digitalWrite(B2, micropasos[i%8][3]);
+  digitalWrite(IN1, micropasos[i%8][0]);
+  digitalWrite(IN2, micropasos[i%8][1]);
+  digitalWrite(IN3, micropasos[i%8][2]);
+  digitalWrite(IN4, micropasos[i%8][3]);
 
   i++;
   delayMicroseconds(T);
